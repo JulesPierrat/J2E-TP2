89fa2f8ac3b32e6dc63c6010d6a05d4c9d6c6812
JulesPierrat <jules.pierrat@ensg.eu>
2022-01-05 15:01:29 +0100

    Ex1

--- src/main/java/tsi/ensg/jee/tp2/simple/QuestionService.java
+++ src/main/java/tsi/ensg/jee/tp2/simple/QuestionService.java
@@ -8,4 +8,15 @@ public class QuestionService {
     public QuestionService(Questions questions){
         this.questions = questions;
     }
+
+    // Getters
+    public String getQuestion(){
+        int index = (int)(Math.random() * this.questions.questions().size());
+        return this.questions.questions().get(index);
+    }
+
+    // Setters
+    public void setQuestions(Questions questions){
+        this.questions = questions;
+    }
 }
